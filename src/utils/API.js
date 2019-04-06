ea9800d5da055cdbf938e74d0ba320207a757c34
Fabio <heloflyer@hotmail.com>
2019-04-06 14:04:30 -0700

    fixed api permissions

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASEURL  = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
-const APIKEY   = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+const APIKEY   = process.env.API_KEY;
 const queryFix = "&q=";
 
 const API = {
